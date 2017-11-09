8f0f3955deb47d68cce5a4d9ebdd6ca60bcbe946
Sarah <sarah.koziol1991@gmail.com>
2017-11-09 09:43:02 +0100

    newVersion; getting started

---
+++ src/test/java/org/geotools/tutorial/feature/AppTest.java
@@ -0,0 +1,21 @@
+package org.geotools.tutorial.feature;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(Arquillian.class)
+public class AppTest {
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClass(App.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+}
